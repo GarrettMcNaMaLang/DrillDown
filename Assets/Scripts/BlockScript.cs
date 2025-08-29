421f3ce249dc8b7e26d3ce32f8dba6157007fb5e
GarrettMcNaMaLang <garrett.m.lang@outlook.com>
2025-08-29 15:32:50 -0400

    Can Rotate and Stop Rotating on Platform
    Might Make a function that takes in a list of disruptors and a boolean, the latter determining if the list contents are to be turned on or off
    
    Function should also exist to turn off whatever the currently on List is

--- Assets/Scripts/BlockScript.cs
+++ Assets/Scripts/BlockScript.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 
 public class BlockScript : MonoBehaviour
@@ -8,7 +10,7 @@ public class BlockScript : MonoBehaviour
 
     public GameObject blockObject;
 
-    bool left, right;
+    bool left, right, grounded;
 
     [SerializeField]
     public List<FallDisruptor> TopDisruptors = new List<FallDisruptor>();
@@ -20,8 +22,23 @@ public class BlockScript : MonoBehaviour
     public List<FallDisruptor> BottomDisruptors = new List<FallDisruptor>();
 
 
+    public enum CurrDisruptors
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    public CurrDisruptors currDisruptors;
+
+
     void RotateBlock(float value)
     {
+        if (grounded == true)
+            return;
+
+
         blockObject.transform.Rotate(new Vector3(0, 0, value));
     }
 
@@ -29,12 +46,16 @@ public class BlockScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if (grounded)
+            StationaryMode();
+
         if (Input.GetKeyDown(KeyCode.A))
         {
             RotateBlock(-90);
@@ -46,5 +67,57 @@ public class BlockScript : MonoBehaviour
         {
             RotateBlock(90);
         }
+
+
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SendCommand(TopDisruptors);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SendCommand(BottomDisruptors);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            SendCommand(LeftDisruptors);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SendCommand(RightDisruptors);
+        }
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            Debug.Log("Block is on Ground");
+            grounded = true;
+        }
+    }
+
+
+    void StationaryMode()
+    {
+
+    }
+
+    void SendCommand(List<FallDisruptor> TurnOn)
+    {
+        foreach (FallDisruptor dis in TurnOn)
+        {
+            dis.inControl = true;
+        }
+    }
+
+    void TurnOff(CurrDisruptors TurnOff)
+    {
+
+    }
+
+    //functions to turn on and off each Disruptor List
 }
--- Assets/Scripts/FallDisruptor.cs
+++ Assets/Scripts/FallDisruptor.cs
@@ -14,7 +14,8 @@ public class FallDisruptor : MonoBehaviour
 
     public DisruptorType disruptorType;
 
-    private bool upTop, inControl;
+    public bool upTop, inControl;
+    
 
     void OnEnable()
     {
@@ -30,11 +31,29 @@ public class FallDisruptor : MonoBehaviour
     {
         
     }
-    
+
     // Update is called once per frame
     void Update()
     {
+        if (disruptorType == DisruptorType.Parachute && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            ParachuteMode();
+        }
 
+        if (disruptorType == DisruptorType.Booster && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            BoosterMode();
+        }
+
+        if (disruptorType == DisruptorType.Booster && Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            BoosterMode();
+        }
+        
+         if (disruptorType == DisruptorType.Booster && Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            BoosterMode();
+        }
     }
 
     bool ParachuteMode()
